f1d37e3f81d4b38e2251d2b2270971f5e0ff0c26
Björn Stresing <bjoern.stresing@gmx.de>
2024-08-18 15:36:04 +0200

    Extract appimagetool so it works in docker

--- conanfile.py
+++ conanfile.py
@@ -38,22 +38,25 @@ class AppImageToolConan(ConanFile):
     # ---Folders---
     no_copy_source = True
 
+    valid_arch = ["x86_64", "x86", "armv6", "armv7", "armv8"]
+
     def validate(self):
         valid_os = ["Linux"]
         if str(self.settings.os) not in valid_os:
             raise ConanInvalidConfiguration(f"{self.name} {self.version} is only supported for the following operating systems: {valid_os}")
-        valid_arch = ["x86_64", "x86", "armv6", "armv7", "armv8"]
-        if str(self.settings.arch) not in valid_arch:
+        if str(self.settings.arch) not in self.valid_arch:
             raise ConanInvalidConfiguration(f"{self.name} {self.version} is only supported for the following architectures on {self.settings.os}: {valid_arch}")
 
     def build(self):
+        appimagetool = self.conan_data["sources"][self.version]["tool"][str(self.settings.arch)]["filename"]
         download(self, **self.conan_data["sources"][self.version]["tool"][str(self.settings.arch)])
-        self.run("chmod +x ./appimagetool-%s.AppImage" % str(self.settings.arch))
-        self.run("./appimagetool-%s.AppImage --appimage-extract" % str(self.settings.arch))
-        rm(self, "appimagetool-%s.AppImage" % str(self.settings.arch), ".")
-        valid_arch = ["x86_64", "x86", "armv6", "armv7", "armv8"]
-        for arch in valid_arch:
+        self.run("chmod +x ./%s" % appimagetool)
+        self.run("./%s --appimage-extract" % appimagetool)
+        rm(self, "%s" % appimagetool, ".")
+        for arch in self.valid_arch:
+            runtime = self.conan_data["sources"][self.version]["runtime"][arch]["filename"]
             download(self, **self.conan_data["sources"][self.version]["runtime"][arch])
+            self.run("chmod +x %s" % runtime)
 
     def package(self):
         copy(self, pattern="*", src=self.build_folder, dst=self.package_folder)
@@ -62,3 +65,6 @@ class AppImageToolConan(ConanFile):
         appimage_bin = os.path.join(self.package_folder, "squashfs-root", "usr", "bin")
         self.output.info('Prepending to PATH environment variable: %s' % appimage_bin)
         self.buildenv_info.prepend_path("PATH", appimage_bin)
+        self.cpp_info.bindirs = ['squashfs-root/usr/bin', './']
+        self.cpp_info.includedirs = []
+        self.cpp_info.libdirs = []
